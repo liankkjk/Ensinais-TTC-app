574fd4a956c28bdc0e59fa663eb58f662a5d2599
anabanana-dev <anaribeiros1804@gmail.com>
2025-04-06 21:05:17 -0300

    tela de perfil feita

--- src/screens/Perfil.tsx
+++ src/screens/Perfil.tsx
@@ -1,14 +1,38 @@
 import React from "react";
-import { View, Text, Button, StyleSheet } from "react-native";
+import { View, Text, Button, StyleSheet, SafeAreaView, StatusBar, Image } from "react-native";
+import { Icon } from "react-native-paper";
 
 const Profile = ({ navigation }: any) => {
   return (
-    <View style={styles.container}>
-      <Text style={styles.title}>Tela de Perfil</Text>
+    <SafeAreaView style={styles.container}>
+      
+      <Text style={styles.title}>Usuário</Text>
+      <Button title="" onPress={() => navigation.openDrawer()} />
+      
+      <StatusBar backgroundColor= "#32CD32" />
 
-      {/* Botão para abrir o menu Drawer */}
-      <Button title="Abrir Menu" onPress={() => navigation.openDrawer()} />
-    </View>
+      <View style={{ width: "100%"}}>
+
+        <Image
+        source={require("assets/fundo-perfil.jpg")}
+        resizeMode='cover'
+        style={{ width: "100%", height: 228 }}
+        />
+
+      </View>
+
+      <View style={{ flex: 1, alignItems: "center"}}>
+
+        <Image
+        source={require("assets/user-perfil.png")}
+        resizeMode='contain'
+        style={{ width: 155, height: 155, borderRadius: 999, borderColor: "#000080", borderWidth: 2, marginTop: -90 }}
+        />
+        <Text>Jonathan</Text>
+      </View>
+      
+      
+    </SafeAreaView>
   );
 };
 
@@ -22,6 +46,12 @@ const styles = StyleSheet.create({
     fontSize: 20,
     fontWeight: "bold",
   },
+  text: {
+    fontSize: 16,
+    marginBottom: 10,
+    color: "#000000",
+    marginVertical: 8,
+  }
 });
 
 export default Profile;
