7a0ec6286e5de57feb14230c1277278ae372644b
Enzo Lian <enzolian41@gmail.com>
2025-04-26 03:40:41 -0300

    Adicionei o campo onde ira mostrar o trofeu conquistado pelo usuário na tela de perfil, somente linkar com o firestore e fazer a lógica de conquistar trofeu por nivel.

--- src/screens/Perfil.tsx
+++ src/screens/Perfil.tsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from "react";
-import { View, Text, Image, SafeAreaView, TouchableOpacity } from "react-native";
+import { View, Text, Image, SafeAreaView, TouchableOpacity, ScrollView } from "react-native";
 import { DrawerActions } from '@react-navigation/native';
 import { Ionicons } from '@expo/vector-icons';
 import styles from "../styles/stylePerfil";
@@ -8,7 +8,6 @@ const Profile = ({ navigation, route }: any) => {
   const [nome, setNome] = useState("Jonathan");
   const [fotoPerfil, setFotoPerfil] = useState(null);
 
-  // Atualiza dados se vierem da tela de edição
   useEffect(() => {
     if (route.params) {
       if (route.params.nomeAtualizado) setNome(route.params.nomeAtualizado);
@@ -18,50 +17,61 @@ const Profile = ({ navigation, route }: any) => {
 
   return (
     <SafeAreaView style={styles.container}>
-      <TouchableOpacity
-        style={styles.menuIcon}
-        onPress={() => navigation.dispatch(DrawerActions.openDrawer())}
-      >
-        <Ionicons name="menu" size={80} color="white" />
-      </TouchableOpacity>
-
-      <View style={styles.headerImageContainer}>
-        <Image
-          source={require("../../assets/fundo-perfil.jpg")}
-          resizeMode="cover"
-          style={styles.headerImage}
-        />
-      </View>
-
-      <View style={styles.profileContainer}>
-        <Image
-          source={fotoPerfil ? { uri: fotoPerfil } : require("../../assets/user.jpg")}
-          resizeMode="contain"
-          style={styles.profileImage}
-        />
-        <Text style={styles.profileName}>{nome}</Text>
-
-        {/* Botão editar perfil */}
+      <ScrollView>
         <TouchableOpacity
-          onPress={() => navigation.navigate("EditarPerfil", {
-            nome, // Passando o nome para edição
-            fotoPerfil, // Passando a foto para edição
-          })}
+          style={styles.menuIcon}
+          onPress={() => navigation.dispatch(DrawerActions.openDrawer())}
         >
-          <Text style={styles.editButtonText}>Editar Perfil</Text>
+          <Ionicons name="menu" size={80} color="white" />
         </TouchableOpacity>
 
-        <View style={styles.expContainer}>
-          <View style={styles.expBarBackground}>
-            <View style={styles.expBarFill} />
-            <Text style={styles.expText}>80 EXP / 200 EXP</Text>
-          </View>
+        <View style={styles.headerImageContainer}>
+          <Image
+            source={require("../../assets/fundo-perfil.jpg")}
+            resizeMode="cover"
+            style={styles.headerImage}
+          />
         </View>
 
-        <View style={styles.level}>
-          <Text style={styles.levelText}>Level 20</Text>
+        <View style={styles.profileContainer}>
+          <Image
+            source={fotoPerfil ? { uri: fotoPerfil } : require("../../assets/user.jpg")}
+            resizeMode="contain"
+            style={styles.profileImage}
+          />
+          <Text style={styles.profileName}>{nome}</Text>
+
+          <TouchableOpacity
+            onPress={() => navigation.navigate("EditarPerfil", {
+              nome,
+              fotoPerfil,
+            })}
+          >
+            <Text style={styles.editButtonText}>Editar Perfil</Text>
+          </TouchableOpacity>
+
+          <View style={styles.expContainer}>
+            <View style={styles.expBarBackground}>
+              <View style={styles.expBarFill} />
+              <Text style={styles.expText}>80 EXP / 200 EXP</Text>
+            </View>
+          </View>
+
+          <View style={styles.level}>
+            <Text style={styles.levelText}>Level 20</Text>
+          </View>
+
+          <View style={styles.conq}>
+            <Text style={styles.conqText}>Consquista atual:</Text>
+          </View>
+
+          <Image
+            source={require("../../assets/user.jpg")}
+            resizeMode="contain"
+            style={styles.conqImage}
+          />
         </View>
-      </View>
+      </ScrollView>
     </SafeAreaView>
   );
 };
