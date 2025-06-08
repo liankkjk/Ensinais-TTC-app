4eeacfdc94e9c70dfb1dfe6a59eb7865a557f07c
Enzo Lian <enzolian41@gmail.com>
2025-06-08 03:01:14 -0300

    Adicionei alguns vídeos no sinalário, adicionei os vídeos sem legenda em algumas atividades (além de revisar os vídeos) e adicionei uma funcionalidade (teste) que o app só carrega quando as fontes carregarem.

--- App.tsx
+++ App.tsx
@@ -1,16 +1,45 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useCallback, useState } from 'react';
 import { View } from 'react-native';
 import Routes from './src/routes';
 import * as SplashScreen from 'expo-splash-screen';
+import * as Font from 'expo-font';
+
+SplashScreen.preventAutoHideAsync();
 
 export default function Ensinais() {
+  const [appIsReady, setAppIsReady] = useState(false);
+
   useEffect(() => {
-    async function hideSplash() {
-      await SplashScreen.hideAsync();
+    async function prepare() {
+      try {
+        await Font.loadAsync({
+          'Poppins-Bold': require('../Ensinais-TTC-app/assets/fonts/Poppins-Bold.ttf'),
+          'Poppins-Medium': require('../Ensinais-TTC-app/assets/fonts/Poppins-Medium.ttf'),
+          'Poppins-ExtraBold': require('../Ensinais-TTC-app/assets/fonts/Poppins-ExtraBold.ttf'),
+        });
+      } catch (e) {
+        console.warn(e);
+      } finally {
+        setAppIsReady(true);
+      }
     }
 
-    hideSplash();
+    prepare();
   }, []);
 
-  return <Routes />;
+  const onLayoutRootView = useCallback(async () => {
+    if (appIsReady) {
+      await SplashScreen.hideAsync();
+    }
+  }, [appIsReady]);
+
+  if (!appIsReady) {
+    return null; 
+  }
+
+  return (
+    <View style={{ flex: 1 }} onLayout={onLayoutRootView}>
+      <Routes />
+    </View>
+  );
 }
